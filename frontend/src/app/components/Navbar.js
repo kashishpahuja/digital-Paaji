2e48d0012e2a53ae721fcca70db33f5dc7fc04a6
Kashish Pahuja <147261989+kashishpahuja@users.noreply.github.com>
2024-12-18 05:00:09 +0530

    navbar updated

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -13,56 +13,197 @@ export default function ResponsiveLayout() {
       //   background:
       //     "linear-gradient(135deg, rgb(254,205,165) 0%, rgb(254,45,45) 50%, rgb(107,0,62) 100%)",
       // }}
-      className="p-8 bg-[#ede7da]"
+      className=" bg-[#ede7da]"
     >
-      <div className="flex  items-center justify-between mx-4 lg:mx-12">
-        <div className="flex items-center justify-start  w-[300px]  "
-            onMouseLeave={handleMouseLeave} 
-      
+      <div className="flex  items-center justify-start mx-4 lg:mx-12 h-[140px]">
+        <div
+          className="flex items-center justify-start  w-[300px]  "
+          onMouseLeave={handleMouseLeave}
         >
-          <div className="w-20 h-auto overflow-hidden"
-            onMouseEnter={()=>{handleMouseEnter("logo2")} }
+          <div
+            className="w-16 h-auto overflow-hidden"
+            onMouseEnter={() => {
+              handleMouseEnter("logo2");
+            }}
           >
             <img src="/Images/logo.webp" alt="logo" />
           </div>
-          
-          <div className={`w-52 h-auto transition-all duration-300 ${show === "logo2" ? 'block' : 'hidden'}`}>
+
+          <div
+            className={`w-52 h-auto transition-all duration-300 ${
+              show === "logo2" ? "block" : "hidden"
+            }`}
+          >
             <img src="/Images/logo2.webp" alt="logo2" />
           </div>
         </div>
-        <div className="hidden lg:block p-4 w-[850px] ">
-          <ul className="flex items-center justify-start gap-12 ">
-            <Link href={"/"} onMouseEnter={()=>{handleMouseEnter("1")}}>
-              <li className={`heading font-bungee-inline text-center ${show ==="1"? 'hidden':'block'}`}
+        <div className="hidden lg:block p-4 w-[850px]  ml-16">
+          <ul className="flex items-center justify-start gap-8  h-20 ">
+            <Link
+              href={"/"}
+              onMouseEnter={() => {
+                handleMouseEnter("1");
+              }}
               onMouseLeave={handleMouseLeave}
+              className="w-24 h-auto"
+            >
+              <li
+                className={`heading font-bungee-inline text-center ${
+                  show === "1" ? "hidden" : "block"
+                }`}
               >
                 Digital <br /> Marketing
               </li>
+              <div
+                className={`w-20 h-20  ${show === "1" ? "block" : "hidden"}`}
+              >
+                <img
+                  src="/Images/1.webp"
+                  alt="1"
+                  className={`w-full h-auto  ${
+                    show === "1" ? "block" : "hidden"
+                  }`}
+                />
+              </div>
             </Link>
-            <img src="/Images/1.webp" alt="1" className={`w-12 h-12 ${show === "1" ? 'block' :'hidden'}`}/>
-            <Link href={"/"}>
-              <li className="heading font-bungee-inline text-center">
-                Social Media <br /> Marketing
-              </li>
-            </Link>
-            <Link href={"/"}>
-              <li className="heading font-bungee-inline text-center">
+            <Link
+              href={"/"}
+              onMouseEnter={() => {
+                handleMouseEnter("1");
+              }}
+              onMouseLeave={handleMouseLeave}
+              className="w-24 h-auto"
+            >
+              <li
+                className={`heading font-bungee-inline text-center ${
+                  show === "1" ? "hidden" : "block"
+                }`}
+              >
                 Digital <br /> Marketing
               </li>
+              <div
+                className={`w-20 h-20  ${show === "1" ? "block" : "hidden"}`}
+              >
+                <img
+                  src="/Images/1.webp"
+                  alt="1"
+                  className={`w-full h-auto  ${
+                    show === "1" ? "block" : "hidden"
+                  }`}
+                />
+              </div>
             </Link>
-            <Link href={"/"}>
-              <li className="heading font-bungee-inline text-center">
+            <Link
+              href={"/"}
+              onMouseEnter={() => {
+                handleMouseEnter("1");
+              }}
+              onMouseLeave={handleMouseLeave}
+              className="w-24 h-auto"
+            >
+              <li
+                className={`heading font-bungee-inline text-center ${
+                  show === "1" ? "hidden" : "block"
+                }`}
+              >
                 Digital <br /> Marketing
               </li>
+              <div
+                className={`w-20 h-20  ${show === "1" ? "block" : "hidden"}`}
+              >
+                <img
+                  src="/Images/1.webp"
+                  alt="1"
+                  className={`w-full h-auto  ${
+                    show === "1" ? "block" : "hidden"
+                  }`}
+                />
+              </div>
             </Link>
-            <Link href={"/"}>
-              <li className="heading font-bungee-inline text-center">
+            <Link
+              href={"/"}
+              onMouseEnter={() => {
+                handleMouseEnter("1");
+              }}
+              onMouseLeave={handleMouseLeave}
+              className="w-24 h-auto"
+            >
+              <li
+                className={`heading font-bungee-inline text-center ${
+                  show === "1" ? "hidden" : "block"
+                }`}
+              >
                 Digital <br /> Marketing
               </li>
+              <div
+                className={`w-20 h-20  ${show === "1" ? "block" : "hidden"}`}
+              >
+                <img
+                  src="/Images/1.webp"
+                  alt="1"
+                  className={`w-full h-auto  ${
+                    show === "1" ? "block" : "hidden"
+                  }`}
+                />
+              </div>
+            </Link>
+            <Link
+              href={"/"}
+              onMouseEnter={() => {
+                handleMouseEnter("1");
+              }}
+              onMouseLeave={handleMouseLeave}
+              className="w-24 h-auto"
+            >
+              <li
+                className={`heading font-bungee-inline text-center ${
+                  show === "1" ? "hidden" : "block"
+                }`}
+              >
+                Digital <br /> Marketing
+              </li>
+              <div
+                className={`w-20 h-20  ${show === "1" ? "block" : "hidden"}`}
+              >
+                <img
+                  src="/Images/1.webp"
+                  alt="1"
+                  className={`w-full h-auto  ${
+                    show === "1" ? "block" : "hidden"
+                  }`}
+                />
+              </div>
+            </Link>
+            <Link
+              href={"/"}
+              onMouseEnter={() => {
+                handleMouseEnter("1");
+              }}
+              onMouseLeave={handleMouseLeave}
+              className="w-24 h-auto"
+            >
+              <li
+                className={`heading font-bungee-inline text-center ${
+                  show === "1" ? "hidden" : "block"
+                }`}
+              >
+                Digital <br /> Marketing
+              </li>
+              <div
+                className={`w-20 h-20  ${show === "1" ? "block" : "hidden"}`}
+              >
+                <img
+                  src="/Images/1.webp"
+                  alt="1"
+                  className={`w-full h-auto  ${
+                    show === "1" ? "block" : "hidden"
+                  }`}
+                />
+              </div>
             </Link>
           </ul>
         </div>
-        <div>
+        <div className="ml-52">
           <TbMenu4 className="w-16 h-8 text-[#cc5f4d]" />
         </div>
       </div>
