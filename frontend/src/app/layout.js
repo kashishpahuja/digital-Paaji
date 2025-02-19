23586f7c2a2f918d73716847c10ef052e99c29e9
Kashish Pahuja <147261989+kashishpahuja@users.noreply.github.com>
2025-02-20 05:13:22 +0530

    hvkm

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,24 +5,12 @@ import { useTitle } from "react-use";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
-import Link from "next/link";
 import Image from "next/image";
-import { IoChatbubbleEllipses } from "react-icons/io5";
-
 import Icons from "./components/Icons";
 
 export default function RootLayout({ children }) {
   const [isPageVisible, setIsPageVisible] = useState(true);
   const [isLoading, setIsLoading] = useState(true); // State to track loading
-  const titles = [
-    "DigitalPaaji - No.1 Digital Marketing Design Development Agency",
-    "Please come back!",
-    "I am feeling lonely :-(",
-    "Don't you love me anymore?",
-  ];
-  const [titleIndex, setTitleIndex] = useState(0);
-
-  useTitle(titles[titleIndex]); // Set initial title
 
   useEffect(() => {
     // Simulate loading time
@@ -31,16 +19,13 @@ export default function RootLayout({ children }) {
     return () => clearTimeout(loadingTimeout);
   }, []);
 
-
   useEffect(() => {
     const handleVisibilityChange = () => {
       if (document.hidden) {
         setIsPageVisible(false);
-        setTitleIndex(1);
         updateFavicon("/Images/smile.png");
       } else {
         setIsPageVisible(true);
-        setTitleIndex(0);
         updateFavicon("/Images/favicon.png");
       }
     };
@@ -51,17 +36,6 @@ export default function RootLayout({ children }) {
       document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
   }, []);
-
-  useEffect(() => {
-    if (!isPageVisible) {
-      const interval = setInterval(() => {
-        setTitleIndex((prevIndex) =>
-          prevIndex >= titles.length - 1 ? 1 : prevIndex + 1
-        );
-      }, 2000);
-      return () => clearInterval(interval);
-    }
-  }, [isPageVisible]);
 
   const updateFavicon = (href) => {
     const existingLink = document.querySelector("link[rel='icon']");
@@ -84,8 +58,14 @@ export default function RootLayout({ children }) {
       <body>
         {isLoading ? (
           // Show loading GIF
-          <div className="flex items-center justify-center h-screen ">
-            <Image width={200} height={200} src="/Images/Preloader.gif" alt="Loading..." className="" />
+          <div className="flex items-center justify-center h-screen">
+            <Image
+              width={200}
+              height={200}
+              src="/Images/Preloader.gif"
+              alt="Loading..."
+              className=""
+            />
           </div>
         ) : (
           // Show actual content after loading
@@ -95,7 +75,7 @@ export default function RootLayout({ children }) {
             </div>
             {children}
             <Footer />
-          <Icons/>
+            <Icons />
           </>
         )}
       </body>
