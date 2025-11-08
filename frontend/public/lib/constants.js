05eb82141d41dee158fcb730a340cf5df8f3f74d
DigitalPaaji <147261989+kashishpahuja@users.noreply.github.com>
2025-11-08 16:56:18 +0530

    g

--- frontend/public/lib/constants.js
+++ frontend/public/lib/constants.js
@@ -1,29 +1,3 @@
-// /lib/constants.js
-export const SERVICES = [
-  "Digital Marketing",
-  "Social Media Marketing",
-  "Search Engine Optimization",
-  "Video Production",
-  "Website Development",
-  "Graphic Design",
-];
-
-
-export const CITIES = [
-  "Patiala",
-  "Rajpura",
-  "Nabha",
-  "Samana",
-  "Ambala",
-  "Sirhind - Fatehgarh Sahib",
-  "Morinda",
-  "Khanna",
-  "Banur",
-  "Sanaur"
-];
-
-
-
 // // /lib/constants.js
 // export const SERVICES = [
 //   "Digital Marketing",
@@ -37,12 +11,38 @@ export const CITIES = [
 
 // export const CITIES = [
 //   "Patiala",
-//   "Chandigarh",
-//   "Mohali",
-//   "Ludhiana",
-//   "Nabha",
 //   "Rajpura",
+//   "Nabha",
 //   "Samana",
-//   "Bathinda",
-//   "Panchkula"
+//   "Ambala",
+//   "Sirhind - Fatehgarh Sahib",
+//   "Morinda",
+//   "Khanna",
+//   "Banur",
+//   "Sanaur"
 // ];
+
+
+
+// /lib/constants.js
+export const SERVICES = [
+  "Digital Marketing",
+  "Social Media Marketing",
+  "Search Engine Optimization",
+  "Video Production",
+  "Website Development",
+  "Graphic Design",
+];
+
+
+export const CITIES = [
+  "Patiala",
+  "Chandigarh",
+  "Mohali",
+  "Ludhiana",
+  "Nabha",
+  "Rajpura",
+  "Samana",
+  "Bathinda",
+  "Panchkula"
+];
